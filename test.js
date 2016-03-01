572b1eaa2cec64333ff509d6aaf77d51b791d1a4
Joey Lea <ovdojoey@gmail.com>
2016-02-29 20:30:28 -0500

    updated to include builds and better loops

--- test.js
+++ test.js
@@ -1,11 +1,29 @@
 var DoLoop = require('./index.js');
 var fs = require('fs');
 
+var buildStream = '';
 var doloop = DoLoop()
-  //.cd('test')
-  .loop('*', function(filename, data){ //.js+$
+  .cd('examples/concat')
+  .readEncoding('utf8')
+  .loop('.js+$', function(filename, data){ //.js+$
     if ( filename === 'test.js' ) {
       // fs.rename(file, 'test2.js');
     }
-    console.log(filename);
+    buildStream = buildStream + data;
+    // console.log(filename);
+    // console.log(data);
+  })
+  .build(function(){
+    console.log(buildStream);
+    fs.writeFile('helloworld.txt', buildStream, function (err) {
+      if (err) return console.log(err);
+      console.log('File wrote');
+    });
   });
+
+  // fs.writeFile('helloworld.txt', buildStream, function (err) {
+  //   if (err) return console.log(err);
+  //   console.log('File wrote');
+  // });
+  //
+  // console.log(buildStream);
